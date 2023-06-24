55b3e42cad570fe2a05f92c0a380fe23bb7d42fb
sorawee <veevisorawee@gmail.com>
2023-06-24 14:48:35 +0700

    step 5.5

--- main.js
+++ main.js
@@ -6,13 +6,24 @@ createApp({
     const image = ref("./assets/images/socks_green.jpg");
     const inStock = ref(true);
     const inventory = ref(100);
-    const onSale = ref(true);
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+    const variants = ref([
+      { id: 2234, color: "green" },
+      { id: 2235, color: "blue" },
+    ]);
+    const sizes = ref([
+      { id: 1, size: "S" },
+      { id: 2, size: "M" },
+      { id: 3, size: "L" },
+    ]);
     return {
       product,
       image,
       inStock,
       inventory,
-      onSale,
+      details,
+      variants,
+      sizes,
     };
   },
 }).mount("#app");
