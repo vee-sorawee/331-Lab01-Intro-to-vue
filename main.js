1d6f3a8c942fbcdab2d9f7dca0a27985557c4051
sorawee <veevisorawee@gmail.com>
2023-06-24 14:17:48 +0700

    step 2.5

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const { createApp, ref } = Vue;
 
 createApp({
   setup() {
-    const product = ref("Socks");
+    const product = ref("Boots");
+    const description = ref(
+      "These boots are a perfect blend of style and functionality, designed to provide both comfort and durability. Crafted with meticulous attention to detail, they are made from high-quality materials that ensure long-lasting wear."
+    );
     return {
       product,
+      description,
     };
   },
 }).mount("#app");
