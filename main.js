fb5afd828f005f3c6ae98865b644e7688006b0dd
sorawee <veevisorawee@gmail.com>
2023-06-24 14:29:12 +0700

    step 3.6

--- main.js
+++ main.js
@@ -3,12 +3,16 @@ const { createApp, ref } = Vue;
 createApp({
   setup() {
     const product = ref("Boots");
-    const description = ref(
-      "These boots are a perfect blend of style and functionality, designed to provide both comfort and durability. Crafted with meticulous attention to detail, they are made from high-quality materials that ensure long-lasting wear."
-    );
+    // const description = ref(
+    //   "These boots are a perfect blend of style and functionality, designed to provide both comfort and durability. Crafted with meticulous attention to detail, they are made from high-quality materials that ensure long-lasting wear."
+    // );
+    const image = ref("./assets/images/socks_green.jpg");
+    const link = ref("https://www.camt.cmu.ac.th/");
     return {
       product,
-      description,
+      image,
+      link,
+      //   description,
     };
   },
 }).mount("#app");
