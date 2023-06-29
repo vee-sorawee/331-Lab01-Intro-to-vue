1c90167477f00de40df53c59bd091532b057b339
sorawee <veevisorawee@gmail.com>
2023-06-29 20:42:11 +0700

    step 8.5

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-const { createApp, ref } = Vue;
+const { createApp, ref, computed } = Vue;
 
 createApp({
   setup() {
     const product = ref("Boots");
-    const image = ref("./assets/images/socks_green.jpg");
-    const inStock = ref(true);
+    const brand = ref("SE 331");
+    const onSale = ref(false);
+    // const image = ref("./assets/images/socks_green.jpg");
+    // const inStock = ref(true);
     const inventory = ref(100);
     const details = ref(["50% cotton", "30% wool", "20% polyester"]);
     const variants = ref([
-      { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-      { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+      {
+        id: 2234,
+        color: "green",
+        image: "./assets/images/socks_green.jpg",
+        quaitity: 50,
+      },
+      {
+        id: 2235,
+        color: "blue",
+        image: "./assets/images/socks_blue.jpg",
+        quantity: 0,
+      },
     ]);
+    const selectedVariant = ref(0);
     const cart = ref(0);
     const addToCart = () => {
       cart.value += 1;
@@ -22,9 +35,34 @@ createApp({
     const changeInStock = () => {
       inStock.value = !inStock.value;
     };
+    const title = computed(() => {
+      if (!onSale.value) {
+        console.log(0);
+
+        return brand.value + " " + product.value;
+      }
+      if (onSale.value) {
+        console.log(1);
+        console.log(onSale);
+
+        return brand.value + " " + product.value + " is on sale";
+      }
+    });
+
+    const updateVariant = (index) => {
+      selectedVariant.value = index;
+    };
+
+    const image = computed(() => {
+      return variants.value[selectedVariant.value].image;
+    });
+
+    const inStock = computed(() => {
+      return variants.value[selectedVariant.value].quantity;
+    });
 
     return {
-      product,
+      title,
       image,
       inStock,
       inventory,
@@ -34,6 +72,7 @@ createApp({
       addToCart,
       updateImage,
       changeInStock,
+      updateVariant,
     };
   },
 }).mount("#app");
